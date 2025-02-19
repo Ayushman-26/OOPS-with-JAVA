7973d2499f1be1910e0696efc574df22da32afe8
Ayushman-26 <ayushmannaresh@gmail.com>
2025-02-19 16:03:16 +0530

    JAVA

---
+++ Mmain.java
@@ -0,0 +1,8 @@
+import Balance.Account;  
+
+public class Mmain {
+    public static void main(String[] args) {
+        Account myAccount = new Account(2000.50); 
+        myAccount.Display_Balance(); 
+    }
+}
